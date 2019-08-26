e260389cae6b26c8a7b764cbd04deda821613952
loctran04 <loc.trandang1004@gmail.com>
2019-08-26 09:14:40 +0700

    "change the code"

--- app/src/main/java/com/example/testgitproject/MainActivity.java
+++ app/src/main/java/com/example/testgitproject/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.testgitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnShow;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShow = findViewById(R.id.btnShow);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Xinchao", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
